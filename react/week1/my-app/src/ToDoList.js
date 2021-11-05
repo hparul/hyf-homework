8d7f70fd6f2a9d7826eb79610dea91731987b121
Parul Jain <hparul@gmail.com>
2021-11-05 22:38:44 +0100

    added todo files

---
+++ react/week1/my-app/src/ToDoList.js
@@ -0,0 +1,16 @@
+const ToDoList = [
+  {
+    description: "go out of bed",
+    deadline: "wed, nov 10 2021",
+  },
+  {
+    description: "brush your teeth",
+    deadline: "thur, nov 11 2021",
+  },
+  {
+    description: "eat breakfast",
+    deadline: "fri, nov 12 2021",
+  },
+];
+
+export default ToDoList;
