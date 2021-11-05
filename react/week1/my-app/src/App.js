620bc778c5aa9beb1642595e72c03c0717804769
Parul Jain <hparul@gmail.com>
2021-11-05 22:35:14 +0100

    added app.js file

---
+++ react/week1/my-app/src/App.js
@@ -0,0 +1,33 @@
+//import logo from './logo.svg';
+import './App.css';
+import './ToDoItem.css';
+
+
+
+import ToDoItem from './ToDoItem.js';
+import ToDoList from './ToDoList.js';
+
+
+function App() {
+
+  return (
+
+    <div className="App">
+      <header className="App-header">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIJoynbf7Cqar_m8fr8VWdKIVnDT-oampspA&usqp=CAU" alt=" girl with list" className="App-logo" />
+        <ul>
+          {
+            ToDoList.map(user => {
+              return (
+                <ToDoItem description={user.description} deadline={user.deadline} />
+              );
+            })
+          }
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
